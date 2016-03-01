684c0b9d93dc5e100dd87db4db55d68bd3cd8372
Hannes Sachsenhofer <hannes@sachsenhofer.com>
2016-03-01 09:48:41 +0100

    Move Mono check to EnvironmentUtil class.

---
+++ src/AsyncFriendlyStackTrace/EnvironmentUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncFriendlyStackTrace
+{
+    internal static class EnvironmentUtil
+    {
+        // see http://www.mono-project.com/docs/gui/winforms/porting-winforms-applications/#runtime-conditionals
+        internal static bool IsRunningOnMono()
+        {
+            return Type.GetType("Mono.Runtime") != null;
+        }
+    }
+}
--- src/AsyncFriendlyStackTrace/ExceptionExtensions.cs
+++ src/AsyncFriendlyStackTrace/ExceptionExtensions.cs
@@ -14,7 +14,7 @@ namespace AsyncFriendlyStackTrace
         private const string AsyncStackTraceExceptionData = "AsyncFriendlyStackTrace";
 
         private static Func<Exception, string> GetStackTraceString => 
-            ReflectionUtil.GenerateGetField<Exception, string>(IsRunningOnMono() ? "stack_trace" : "_stackTraceString");
+            ReflectionUtil.GenerateGetField<Exception, string>(EnvironmentUtil.IsRunningOnMono() ? "stack_trace" : "_stackTraceString");
 
         private static readonly Func<Exception, string> GetRemoteStackTraceString =
             ReflectionUtil.GenerateGetField<Exception, string>("_remoteStackTraceString");
@@ -116,12 +116,6 @@ namespace AsyncFriendlyStackTrace
                              new StackTrace(exception, true).ToAsyncString();
             var remoteStackTrace = GetRemoteStackTraceString(exception);
             return remoteStackTrace + stackTrace;
-        }
-		
-	// see http://www.mono-project.com/docs/gui/winforms/porting-winforms-applications/#runtime-conditionals
-        private static bool IsRunningOnMono()
-        {
-            return Type.GetType("Mono.Runtime") != null;
         }
     }
 }
